be24ef7017e9a50bd5c9b856fa031f75660b2ea5
Tianyi <tianyiwang666@gmail.com>
2022-10-06 16:55:49 -0400

    init the baisc structures

--- cpp/wavefront_reconstruction/cwfr.cpp
+++ cpp/wavefront_reconstruction/cwfr.cpp
@@ -6,11 +6,34 @@
 #include "cwfr.h"
 
 
-// This is the constructor of a class that has been exported.
-CWFR::CWFR()
+CWFR::~CWFR()
 {
 }
 
-CWFR::~CWFR()
+MatrixXXd CWFR::operator()(const MatrixXXd& Sx, const MatrixXXd& Sy, const MatrixXXd& X, const MatrixXXd& Y, WFR_METHOD method)
 {
+	switch (method)
+	{
+	case WFR_METHOD::HFLI:
+		return hfli(Sx, Sy, X, Y);
+		break;
+	case WFR_METHOD::HFLIQ:
+		break;
+	default:
+		return MatrixXXd();
+		break;
+	}
+}
+
+MatrixXXd CWFR::hfli(const MatrixXXd& Sx, const MatrixXXd& Sy, const MatrixXXd& X, const MatrixXXd& Y)
+{
+	// 0. build the least-squares system
+	// 0.0 Pre-allocate or reserve the required space
+
+
+
+	// 1. solve the least-squares system
+
+	// 2. build and return the result
+	return MatrixXXd();
 }
--- cpp/wavefront_reconstruction/cwfr.h
+++ cpp/wavefront_reconstruction/cwfr.h
@@ -1,9 +1,6 @@
-// The following ifdef block is the standard way of creating macros which make exporting
-// from a DLL simpler. All files within this DLL are compiled with the WAVEFRONTRECONSTRUCTION_EXPORTS
-// symbol defined on the command line. This symbol should not be defined on any project
-// that uses this DLL. This way any other project whose source files include this file see
-// WAVEFRONTRECONSTRUCTION_API functions as being imported from a DLL, whereas this DLL sees symbols
-// defined with this macro as being exported.
+#ifndef CWFR_H
+#define CWFR_H
+
 #ifdef WAVEFRONTRECONSTRUCTION_EXPORTS
 #define WAVEFRONTRECONSTRUCTION_API __declspec(dllexport)
 #else
@@ -18,13 +15,63 @@ using MatrixXXd = Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::R
 using VectorXd = Eigen::Vector<double, Eigen::Dynamic>;
 
 
-// This class is exported from the dll
+//! This is the class the reconstruct the wavefront shape from gradient data
+/*!
+* Reference:
+	[1] Guanghui Li, Yanqiu Li, Ke Liu, Xu Ma, and Hai Wang, "Improving
+	wavefront reconstruction accuracy by using integration equations with
+	higher-order truncation errors in the Southwell geometry," J. Opt. Soc.
+	Am. A 30, 1448-1459 (2013)
+	[2] Lei Huang, Junpeng Xue, Bo Gao, Chao Zuo, and Mourad Idir,"Zonal
+	wavefront reconstruction in quadrilateral geometry for phase measuring
+	deflectometry," Appl. Opt. 56, 5139-5144 (2017)
+*/
 class WAVEFRONTRECONSTRUCTION_API CWFR {
 public:
-	CWFR(void);
+	enum class WFR_METHOD {
+		HFLI,
+		HFLIQ,
+	};
+
+public:
+	CWFR() = default;
 	virtual ~CWFR();
+
+	// Disable copyping
+	CWFR(const CWFR&) = delete;
+	CWFR& operator=(const CWFR&) = delete;
+
+	MatrixXXd operator () (
+		const MatrixXXd& Sx,/*!< [in] Slopes in x direction*/
+		const MatrixXXd& Sy,/*!< [in] Slopes in y direction*/
+		const MatrixXXd& X, /*!< [in] x coordinates*/
+		const MatrixXXd& Y, /*!< [in] y coordinates*/
+		WFR_METHOD method = WFR_METHOD::HFLI /*!< [in] method to be used*/
+	);
+
+private:
+	//! HFLI method
+	/*!
+	* Reconstruct the height from the slopes in x and y directions with
+	* the High-order Finite-difference-based Least-squares Integration (HFLI)
+	* method.
+	*					D * z = g
+	* \return the reconstructed wavefront Z
+	*/
+	MatrixXXd hfli(
+		const MatrixXXd& Sx,/*!< [in] Slopes in x direction*/
+		const MatrixXXd& Sy,/*!< [in] Slopes in y direction*/
+		const MatrixXXd& X, /*!< [in] x coordinates*/
+		const MatrixXXd& Y  /*!< [in] y coordinates*/
+	);
+
+private:
+	void preapre_Dzg(
+		
+	);
 };
 
-//extern WAVEFRONTRECONSTRUCTION_API int nwavefrontreconstruction;
-//
-//WAVEFRONTRECONSTRUCTION_API int fnwavefrontreconstruction(void);
+
+#endif // !CWFR_H
+
+
