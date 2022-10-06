b064fe6e28b8e947f297ade4d2c6689728b4465d
Tianyi <tianyiwang666@gmail.com>
2022-10-06 14:50:00 -0400

    initial commit of the cpp version

---
+++ cpp/wavefront_reconstruction/wavefront_reconstruction.h
@@ -0,0 +1,30 @@
+// The following ifdef block is the standard way of creating macros which make exporting
+// from a DLL simpler. All files within this DLL are compiled with the WAVEFRONTRECONSTRUCTION_EXPORTS
+// symbol defined on the command line. This symbol should not be defined on any project
+// that uses this DLL. This way any other project whose source files include this file see
+// WAVEFRONTRECONSTRUCTION_API functions as being imported from a DLL, whereas this DLL sees symbols
+// defined with this macro as being exported.
+#ifdef WAVEFRONTRECONSTRUCTION_EXPORTS
+#define WAVEFRONTRECONSTRUCTION_API __declspec(dllexport)
+#else
+#define WAVEFRONTRECONSTRUCTION_API __declspec(dllimport)
+#endif
+
+// Eigen API aliases
+using int_t = Eigen::Index;
+using Tripletd = Eigen::Triplet<double>;
+using SparseMatrixXXd = Eigen::SparseMatrix<double, Eigen::RowMajor>;
+using MatrixXXd = Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>;
+using VectorXd = Eigen::Vector<double, Eigen::Dynamic>;
+
+
+// This class is exported from the dll
+class WAVEFRONTRECONSTRUCTION_API Cwavefrontreconstruction {
+public:
+	Cwavefrontreconstruction(void);
+	// TODO: add your methods here.
+};
+
+extern WAVEFRONTRECONSTRUCTION_API int nwavefrontreconstruction;
+
+WAVEFRONTRECONSTRUCTION_API int fnwavefrontreconstruction(void);
