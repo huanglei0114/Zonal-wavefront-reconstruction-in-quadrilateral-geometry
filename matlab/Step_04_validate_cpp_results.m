bc0d498afa292cf4557fe887ef4f2e8ed774aa3f
Tianyi <tianyiwang666@gmail.com>
2022-10-13 14:30:58 -0400

    added results

--- matlab/Step_04_validate_cpp_results.m
+++ matlab/Step_04_validate_cpp_results.m
@@ -11,11 +11,11 @@ Z = Load2DMatrix('../data/Z.bin', 'double');
 
 figure;
 subplot(1, 3, 1);
-show_surface_map(X, Y, Z, 0, 'jet', 'flat', true, 2, 1, '', '');
+show_surface_map(X, Y, Z, 0, 'jet', 'flat', true, 2, 1, '', 'Nominal height');
 subplot(1, 3, 2);
-show_surface_map(X, Y, Z_calc, 0, 'jet', 'flat', true, 2, 1, '', '');
+show_surface_map(X, Y, Z_calc, 0, 'jet', 'flat', true, 2, 1, '', 'Integrated height');
 subplot(1, 3, 3);
-show_surface_map(X, Y, Z - Z_calc, 0, 'jet', 'flat', true, 5, 1, '', '');
+show_surface_map(X, Y, Z - Z_calc, 0, 'jet', 'flat', true, 5, 1, '', 'Residual height');
 
 
 g = Load2DMatrix('../data/g.bin', 'double');
